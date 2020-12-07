3aecd42980c976620202cada7d92155137ae0ae1
Siddharth <siddharthmagadum16@gmail.com>
2020-12-07 22:30:50 +0530

    intermediate push

--- server.js
+++ server.js
@@ -19,7 +19,17 @@ const userinfo=mongoose.Schema({
     password: String
 })
 
+const foodList=mongoose.Schema({
+        username: String,
+        name: String,
+        price: Number,
+        rating: Number,
+        place: String,
+        contactno: Number
+})
+
 const Userinfo = mongoose.model('userinfo',userinfo)
+const Foodstuff = mongoose.model('foodstuff',foodList)
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -44,6 +54,13 @@ function deletee(){
 app.post('/register',(req,res)=>{
     let finalres=0
     try{
+        // let valid=0;
+        // for (let i in req.body.username) {
+        //     if(i==="@") valid=1
+        // }
+        // if(valid===0){
+        //     return res.json('0')
+        // }
         Userinfo.insertMany({
             username: req.body.username,
             password: req.body.password
@@ -75,8 +92,7 @@ app.post('/signin',(req,res)=>{
             res.json('0')
         }else{
             console.log(`${result}`)
-            if(result!==null)
-                res.json('1')
+            if(result!==null)   res.json('1')
             else res.json('0')
             // res.json(`userinfo found in db `)
         }
@@ -85,23 +101,26 @@ app.post('/signin',(req,res)=>{
 
 let foods=[
     food1= {
+        id: 1,
         name: 'pizza',
         price: 100,
-        stars: 3.5/5,
+        rating: 3.5/5,
         place: 'Dwarka Nagar, Banglore',
         contactno: 948569712,
     },
     food2={
+        id: 2,
         name: 'North Indian Biryani',
         price: 100,
-        stars: 3.5/5,
+        rating: 3.5/5,
         place: 'Dwarka Nagar2, Banglore',
         contactno: 948569712,
     },
     food3= {
+        id: 3,
         name: 'Chinese schezwan fried rice',
         price: 100,
-        stars: 3.5/5,
+        rating: 3.5/5,
         place: 'Dwarka Nagar3, Banglore',
         contactno: 948569712,
     }
@@ -110,8 +129,80 @@ let foods=[
 
 app.get('/home/buy',(req,res)=>{
     // foods=0
-    res.json(foods)
+
+    res.send(foods)
 })
+
+app.get('./home/sell',(req,res)=>{
+    Foodstuff.insertMany({
+        username:req.body.username,
+        name: req.body.name,
+        price: req.body.price,
+        rating: req.body.rating,
+        place: req.body.place,
+        contactno: req.body.contactno
+    },(err,result)=>{
+        if(err) res.send("0")
+        else{
+            console.log(result)
+            res.send("1")
+        }
+    })
+})
+
+async function findFoodsbySeller(username){
+    let foodbyseller= await Foodstuff.find({username : username})
+    console.log(foodbyseller)
+    return foodbyseller;
+}
+
+app.get('/home/sell/:username',(req,res)=>{
+    console.log("username:  "+req.params.username)
+    // sendList=  findFoodsbySeller(req.params.username)
+    // Foodstuff.findMany({'username':req.params.username},(err,result)=>{
+    //     if(err) res.status(404).send("0")
+    //     else{
+    //         res.send(result)
+    //     }
+    // })
+    Foodstuff
+    .find()
+    .or([{username:req.params.username}])
+    .then(result=>{
+        console.log(result);
+        res.send(result)
+    })  
+    .catch(err=>{
+        console.log(`err occured ${err}`)
+        res.status(404).send("0")
+    })
+
+    // foodiedb.Foodstuff
+
+
+    // let foodbyseller= Foodstuff.find({username : req.params.username})
+    // console.log(foodbyseller)
+    // res.send(foodbyseller)
+})
+
+// temperoary
+function InsertFoodstuff(){
+    try{
+
+        Foodstuff.insertMany({
+            username:'sid',
+            name: 'Gobi Manchuri',
+            price: '50',
+            rating: 4.5,
+            place: 'Ashok Nagar',
+            contactno: 9478219354
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// InsertFoodstuff();
 
 
 app.get('*',(req,res)=>{
