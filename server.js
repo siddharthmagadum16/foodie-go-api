8922053c691ade9916b4c223de21375fb87b6830
Siddharth Magadum <siddharthmagadum16@gmail.com>
2021-08-25 19:08:19 +0530

    Resolving email bug

--- server.js
+++ server.js
@@ -22,9 +22,22 @@ mongoose.set('useNewUrlParser', true);
 mongoose.connect(database_url)
 .then(res=> console.log(`successuflly connected: ${res} ${database_url}`))
 .catch(err=>console.log(`error connecting to db : ${err}`));
-
-
 app.use(cors())
+
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://127.0.0.1:3001', 'http://localhost:3001', 'https://foodie-go.herokuapp.com', ''];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // when data is sent in urlencoded format
 
