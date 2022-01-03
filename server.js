22990b0d1d6875832c6b2795713d31097d8b8443
Siddharth Magadum <siddharthmagadum16@gmail.com>
2022-01-03 18:25:56 +0530

    removed middleware

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app= express()
 
-
-
 var database_url;
 
 if(process.env.ENV === 'PRODUCTION'){
@@ -20,22 +18,8 @@ mongoose.set('useNewUrlParser', true);
 mongoose.connect(database_url)
 .then(res=> console.log(`successuflly connected: ${res} ${database_url}`))
 .catch(err=>console.log(`error connecting to db : ${err}`));
+
 app.use(cors())
-
-app.use((req, res, next) => {
-    const allowedOrigins = ['http://127.0.0.1:3001', 'http://localhost:3001', 'https://foodie-go.herokuapp.com', ''];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-});
-
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // when data is sent in urlencoded format
 
@@ -51,6 +35,5 @@ const port= process.env.PORT || 4000
 
 app.listen(port,()=>{
     console.log("App is listening on port "+ port)
-    // console.log(`Error listening port ${err}`)
 })
 
